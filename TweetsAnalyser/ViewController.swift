62f8e15e9fef62bc0ee4f0749909d8a2a63cf16d
aanchalpatial <apatial.27@gmail.com>
2019-07-01 11:29:35 +0530

    Twitter Sentiment Model created using Create ML. Incorporated above model in the project. Updated UI.

--- TweetsAnalyser/ViewController.swift
+++ TweetsAnalyser/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func analyseButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
