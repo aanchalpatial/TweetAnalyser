1943f2cf587fa8c1b2cfa4fa1f6b88c01c3c01d8
aanchalpatial <apatial.27@gmail.com>
2019-07-02 02:14:22 +0530

    Used ML Model to generate predictions and calculate sentiment score. Update UI Accordingly.

--- TweetsAnalyser/ViewController.swift
+++ TweetsAnalyser/ViewController.swift
@@ -12,41 +12,85 @@ import SwiftyJSON
 import CoreML
 
 class ViewController: UIViewController {
+    
+    //MARK: Variables
 
     @IBOutlet weak var textField: UITextField!
-    
     @IBOutlet weak var emojiLabel: UILabel!
-
+    @IBOutlet weak var analyseButton: UIButton!
+    
     // Instantiation using Twitter's OAuth Consumer Key and secret
     let swifter = Swifter(consumerKey: "AoaMzhn4B4VcCVphkCGu3VrB7", consumerSecret: "LY3pZQDiq7GBBOgecA9qNR1ZisVWj81meTZGDReuhQRg9ngYx9")
     
     let sentimentClassifier = TweetSentimentClassifier()
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode : .extended, success: { (results, metadata) in
-            
-            //JSON Parsing
-            var tweetArray = [String]()
-            for i in 0..<100 {
-                if let tweet = results[i]["full_text"].string{
-                    tweetArray.append(tweet)
-                }
-            }
-            
-            
-            
-        }) { (error) in
-            print("Error with Twitter API requets \(error)")
-        }
+        analyseButton.layer.cornerRadius = 7
+        analyseButton.clipsToBounds = true
         
     }
 
     @IBAction func analyseButtonPressed(_ sender: UIButton) {
+        fetchTweets()
     }
     
+    
+    func fetchTweets() {
+        if let keyword = textField.text {
+            swifter.searchTweet(using: keyword, lang: "en", count: 100, tweetMode : .extended, success: { (results, metadata) in
+                
+                //JSON Parsing
+                var tweetArray = [TweetSentimentClassifierInput]()
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string{
+                        let tweetToAnalyse = TweetSentimentClassifierInput(text: tweet)
+                        tweetArray.append(tweetToAnalyse)
+                    }
+                }
+                self.makePrediction(with: tweetArray)
+                
+            }) { (error) in
+                print("Error with Twitter API requets \(error)")
+            }
+        }
+    }
+    
+    func makePrediction(with tweetArray : [TweetSentimentClassifierInput]) {
+        
+        do{
+            let predictions = try self.sentimentClassifier.predictions(inputs: tweetArray)
+            var score : Int = 0
+            for i in 0..<100 {
+                if predictions[i].label == "Pos" {
+                    score += 1
+                }else if predictions[i].label == "Neg" {
+                    score -= 1
+                }else if predictions[i].label == "Nuetral"{
+                    //do nothing
+                }
+            }
+            
+            updateUI(with: score)
+            
+        }catch {
+            print("Error making prediction.")
+        }
+    }
+    
+    func updateUI(with score : Int){
+        
+        if score > 20 {
+            emojiLabel.text = "😀"
+        }else if score > 10 {
+            emojiLabel.text = "🙂"
+        }else if score > -10 {
+            emojiLabel.text = "😟"
+        }else if score < -10 {
+            emojiLabel.text = "😖"
+        }
+    }
 }
 
