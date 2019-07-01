8b7888e8a5c3573aa4caf5145ecd967c5b8b7b04
aanchalpatial <apatial.27@gmail.com>
2019-07-01 22:34:56 +0530

    Used Swifter Framework to request data from twitter. Included SwiftyJSON Pod.

--- TweetsAnalyser/ViewController.swift
+++ TweetsAnalyser/ViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var emojiLabel: UILabel!
+
+    // Instantiation using Twitter's OAuth Consumer Key and secret
+    let swifter = Swifter(consumerKey: "AoaMzhn4B4VcCVphkCGu3VrB7", consumerSecret: "LY3pZQDiq7GBBOgecA9qNR1ZisVWj81meTZGDReuhQRg9ngYx9")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode : .extended, success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print("Error with Twitter API requets \(error)")
+        }
+        
     }
 
     @IBAction func analyseButtonPressed(_ sender: UIButton) {
